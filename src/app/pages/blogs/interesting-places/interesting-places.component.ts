ecbbfbf4b428bb9061020b3cf3b2c6f9f50b7278
Asad Ali <[asad.ar451@gmail.com]>
2025-06-27 12:40:57 +0500

    Refactor blog components to utilize router for navigation and improve blog display
    - Updated BlogHomeComponent to include router for blog navigation.
    - Removed hardcoded popular posts and added onBlogClick method for dynamic routing.
    - Enhanced CreativeLearningComponent to fetch and display blogs based on category.
    - Modified EducationComponent to filter blogs by category and updated HTML for consistency.
    - Revamped InterestingPlacesComponent to display blogs dynamically with router links.
    - Updated ApiServicesService to include a method for fetching a single blog by ID.
    - Refactored BlogsHeaderComponent to dynamically display blog categories and links.
    - Cleaned up unused code and improved overall structure for better maintainability.

--- src/app/pages/blogs/interesting-places/interesting-places.component.ts
+++ src/app/pages/blogs/interesting-places/interesting-places.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { ApiServicesService } from '../../../services/api-services.service';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-interesting-places',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './interesting-places.component.html',
   styleUrl: './interesting-places.component.css'
 })
 export class InterestingPlacesComponent {
-
+  constructor(private api: ApiServicesService, private router: Router) { }
+  allBlogs: any;
+  ngOnInit() {
+    this.api.getBlogs().subscribe((res: any) => {
+      this.allBlogs = res.data.filter((b: any) => b.category === 'Ciekawe miejsca')
+    })
+  }
+  onBlogClick(id: string) {
+    this.router.navigate(['/blog', id]);
+  }
 }
