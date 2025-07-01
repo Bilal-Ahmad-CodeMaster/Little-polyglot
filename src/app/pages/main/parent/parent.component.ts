43fabbbe59a35c7d10d0ab542fb4f6f01c25413e
Bilal Ahmad <bilal.codemaster@gmail.com>
2025-07-01 17:41:53 +0500

    update just about remaining

--- src/app/pages/main/parent/parent.component.ts
+++ src/app/pages/main/parent/parent.component.ts
@@ -13,46 +13,31 @@ export class ParentComponent {
   cards = [
     {
       icon: 'https://storage.googleapis.com/a1aa/image/f4ef5205-4f75-473d-f306-a62dec847a6f.jpg',
-      titleLine1: 'Login',
+      titleLine1: 'Sign UP',
       titleLine2: 'to Polyglot Kids',
-      description: 'Log in to Polyglot Kids Online and use all the online tools we have prepared for students and parents.',
-      linkText: 'Log in'
+      description: 'SignUp to Polyglot Kids to explore new things for your children and learn english from professionals',
+      linkText: 'SignUp',
+      link: "/registration/applicationForm"
     },
     {
       icon: 'https://storage.googleapis.com/a1aa/image/eecab23f-bfb5-4eb1-dd42-cc71ecb9f0fc.jpg',
       titleLine1: 'Smart Parent',
       titleLine2: 'Academy',
       description: 'Smart Parent Academy is materials for parents developed by experts: webinars and blog articles.',
-      linkText: 'More'
+      linkText: 'More',
+      link: '/parent/smart-parent-academy'
     },
+
+
     {
       icon: 'https://storage.googleapis.com/a1aa/image/a15d0c59-0db1-4c8f-a202-c3be8bc4516f.jpg',
-      titleLine1: 'GRAMMAR RHYMES',
-      titleLine2: 'Manual',
-      description: 'GRAMMAR RHYMES – our original book for learning English grammar for children aged 8-14.',
-      linkText: 'More'
-    },
-    {
-      icon: 'https://storage.googleapis.com/a1aa/image/f4ef5205-4f75-473d-f306-a62dec847a6f.jpg',
-      titleLine1: 'Login',
-      titleLine2: 'to Polyglot Kids',
-      description: 'Log in to Polyglot Kids Online and use all the online tools we have prepared for students and parents.',
-      linkText: 'Log in'
-    },
-    {
-      icon: 'https://storage.googleapis.com/a1aa/image/eecab23f-bfb5-4eb1-dd42-cc71ecb9f0fc.jpg',
-      titleLine1: 'Smart Parent',
-      titleLine2: 'Academy',
-      description: 'Smart Parent Academy is materials for parents developed by experts: webinars and blog articles.',
-      linkText: 'More'
-    },
-    {
-      icon: 'https://storage.googleapis.com/a1aa/image/a15d0c59-0db1-4c8f-a202-c3be8bc4516f.jpg',
-      titleLine1: 'GRAMMAR RHYMES',
-      titleLine2: 'Manual',
-      description: 'GRAMMAR RHYMES – our original book for learning English grammar for children aged 8-14.',
-      linkText: 'More'
+      titleLine1: 'Articles/Blogs',
+      titleLine2: 'Learn It',
+      description: 'Discover fun, educational articles for parents and students! From language tips to creative activities, explore how learning English can be engaging and inspiring.',
+      linkText: 'More',
+      link: '/blogs'
     }
+
   ];
 
 
